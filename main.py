fdb71d66dd262c6b666b5b577594000ff2961b97
FruFruJ <jovananikolicdd98@gmail.com>
2021-02-24 12:59:45 +0100

    hi

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ from geneticalgorithm import geneticalgorithm as ga
 
 
 
-
-
-
 def mainFunction():
     cena = [1000, 500, 200, 700, 50, 70, 100, 300, 5000, 1500, 200, 600]
-    vaznost = [10, 2, 5, 2, 1, 6, 7, 3, 9, 10, 8, 3]
+    #vaznost = [10, 2, 5, 2, 1, 6, 7, 3, 9, 10, 8, 3]
     predmeti = ["jelka za sto", "ukras za vrh jelke", "komplet ukrasa malih", "komplet ukrasa veliki",
                 "jedan ukras deda mraz", "jedan ukras irvas", "jedan ukras zvezda", "masna za unutra i spolja",
                 "velika jelka", "crevo duzina 8m", "sijalice 3m", "sijalice 8m"];
-    print("Dobar dan pomoci cemo vam u vasoj optimalnoj dekoraciji kuce ukrasima")
+    print("Dobar dan svimaa pomoci cemo vam u vasoj optimalnoj dekoraciji kuce ukrasima")
     budzet=int(input("Unesite vas budzet \n"));
     vaznost=[];
     for x in predmeti:
@@ -34,27 +31,27 @@ def mainFunction():
             ukCena += x[i] * cena[i];
             #print(i)
             i = i + 1
-        #print(ukCena)
+        print(ukCena)
         if (ukCena > budzet):
             return 0;
         return -fit
 
-    varbound = np.array([[0, 1]] * 12)
+    mogucePromenljive = np.array([[0, 1]] * 12)
 
-    algorithm_param = {'max_num_iteration': 100, \
-                       'population_size': 100, \
+    parametri = {'max_num_iteration': 150, \
+                       'population_size': 300, \
                        'mutation_probability': 0.1, \
                        'elit_ratio': 0.0, \
+                       'crossover_type': 'two_point', \
                        'crossover_probability': 0.5, \
-                       'parents_portion': 0.2, \
-                       'crossover_type': 'one_point', \
+                       'parents_portion': 0.1, \
                        'max_iteration_without_improv': None}
 
     model = ga(function=f, \
                dimension=12, \
                variable_type='int', \
-               variable_boundaries=varbound, \
-               algorithm_parameters=algorithm_param)
+               variable_boundaries=mogucePromenljive,\
+               algorithm_parameters=parametri)
 
     model.run()
     print(model.output_dict)
